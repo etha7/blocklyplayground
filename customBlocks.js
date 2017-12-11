93a84dac1ce2168bed07422855a64684d48a462c
Ethan DeSon <ethandeson@gmail.com>
2017-12-10 22:23:15 -0800

    finished project 3

--- customBlocks.js
+++ customBlocks.js
@@ -28,6 +28,21 @@ Blockly.Blocks['clearscreen'] = {
     }
 };
 
+Blockly.Blocks['say'] = {
+
+    init: function() {
+        this.appendValueInput("Say")
+            .setCheck("String")
+            .appendField("Say:");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(335);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
 Blockly.Blocks['catpose'] = {
     init: function() {
         this.appendDummyInput()
@@ -36,6 +51,19 @@ Blockly.Blocks['catpose'] = {
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dogpose'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Change Pose")
+            .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "dogIndex");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
         this.setTooltip("");
         this.setHelpUrl("");
     }
@@ -52,8 +80,22 @@ Blockly.JavaScript['clearscreen'] = function(block) {
     return blockCode;
 };
 
+
+Blockly.JavaScript['say'] = function(block) {
+  var value_say = Blockly.JavaScript.valueToCode(block, 'Say', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'await sleep(1000);say('+value_say+');await sleep(1000);say(\'\');';
+  return code;
+};
+
 Blockly.JavaScript['catpose'] = function(block) {
     var dropdown_catindex = block.getFieldValue('catIndex');
-    var blockCode = 'updatePose('+dropdown_catindex+');';
+    var blockCode = 'await sleep(1000); updatePose('+dropdown_catindex+', '+'\'cat\''+');';
+    return blockCode;
+};
+
+
+Blockly.JavaScript['dogpose'] = function(block) {
+    var dropdown_dogindex = block.getFieldValue('dogIndex');
+    var blockCode = 'await sleep(1000); updatePose('+dropdown_dogindex+', '+'\'dog\''+');';
     return blockCode;
 };
